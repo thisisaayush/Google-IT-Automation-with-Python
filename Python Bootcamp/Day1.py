a38c6fca6da37b441a8f9ca9936ae45105bcc867
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2021-07-01 08:28:37 -0500

    Add files via upload

---
+++ Python Bootcamp/Day1.py
@@ -0,0 +1,4 @@
+print("Hello World!")#inside the parenthesis, there is a string - Hello World!, resided in double quotes.
+print("Day 1 - Python Print Function")
+print("print('what to print')")
+print("Hello World!\nHello Universe!")
