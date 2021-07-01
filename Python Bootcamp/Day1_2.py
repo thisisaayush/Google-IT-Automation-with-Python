b1f9c0bd995f079d9f0d586a1d627573d62247f7
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2021-07-01 08:28:57 -0500

    Add files via upload

---
+++ Python Bootcamp/Day1_2.py
@@ -0,0 +1,7 @@
+str = input("What is your name?\n")
+n = len(str)
+print("Hello "+  str +"! How may I help you?")
+print("Find the length of my name.")
+print("Sure!!!",str)
+if(n > 0):
+    print("The length of your name is ",n,".")
